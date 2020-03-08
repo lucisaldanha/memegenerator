fe6d6660831ba620abef91b0662fa7ab47301be1
Lucieni Saldanha <lucieni.saldanha@gmail.com>
2020-03-07 21:01:17 -0600

    Updates

---
+++ precache-manifest.89c62cdeed781dc9bedbc02c9f7d67c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1da69515364b0c93647306624eb4b500",
+    "url": "/memegenerator/index.html"
+  },
+  {
+    "revision": "e4593b770c4fdcd01124",
+    "url": "/memegenerator/static/js/2.038a1c50.chunk.js"
+  },
+  {
+    "revision": "809f8196affeaac0e5176b72b79c267c",
+    "url": "/memegenerator/static/js/2.038a1c50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5144c80ece794011dc9d",
+    "url": "/memegenerator/static/js/main.740b3ff2.chunk.js"
+  },
+  {
+    "revision": "d9243b8f250b27610d7d",
+    "url": "/memegenerator/static/js/runtime-main.50adb4fc.js"
+  }
+]);
